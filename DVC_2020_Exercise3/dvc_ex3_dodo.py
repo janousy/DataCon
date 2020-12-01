7dfe1d049b530f223a174394e7592e699755ce90
Janosch Baltensperger <janosch.baltensperger@uzh.ch>
2020-12-01 16:46:59 +0100

    start exercise 4

--- DVC_2020_Exercise3/dvc_ex3_dodo.py
+++ DVC_2020_Exercise3/dvc_ex3_dodo.py
@@ -8,6 +8,7 @@ from bokeh.transform import linear_cmap
 from bokeh.layouts import gridplot
 
 
+
 # ==========================================================================
 # Goal: Visualize Covid-19 Tests statistics in Switzerland with linked plots
 # Dataset: covid19_tests_switzerland_bag.csv
@@ -25,32 +26,35 @@ from bokeh.layouts import gridplot
 
 ## T1.1 Read the data to the dataframe "raw"
 # You can read the latest data from the url, or use the data provided in the folder (update Nov.3, 2020)
-url = 'https://github.com/daenuprobst/covid19-cases-switzerland/blob/master/covid19_tests_switzerland_bag.csv'
-raw = pd.read_csv('covid19_tests_switzerland_bag.csv',index_col=[0])
-print(raw.head())
+
+url = 'https://raw.githubusercontent.com/daenuprobst/covid19-cases-switzerland/master/covid19_tests_switzerland_bag.csv'
+fix_data = 'covid19_tests_switzerland_bag.csv'
+raw = pd.read_csv(url)
+
 
 
 ## T1.2 Create a ColumnDataSource containing: date, positive number, positive rate, total tests
 # All the data can be extracted from the raw dataframe.
 
-date = raw.date 
+date = raw.date
 pos_num = raw.n_positive
-pos_rate = raw.frac_positive
+pos_rate = raw.frac_negative
 test_num = raw.n_tests
 
 
 source = ColumnDataSource(data=dict(
-    x=pd.to_datetime(date),
-    test_num=test_num,
-    pos_num=pos_num,
-    pos_rate=pos_rate,
+    date=pd.to_datetime(date),
+    pos_num = pos_num,
+    pos_rate = pos_rate,
+    test_num = test_num
 ))
+
 
 
 ## T1.3 Map the range of positive rate to a colormap using module "linear_cmap"
 # "low" should be the minimum value of positive rates, and "high" should be the maximum value
 
-mapper = linear_cmap("P-Rate", bp.magma(len(pos_rate.unique())), pos_rate.min(), pos_rate.max(), low_color=None, high_color=None, nan_color='gray')
+mapper = linear_cmap(field_name=source.data['pos_rate'],palette=bp.Inferno256, low=pos_rate.min(), high=pos_rate.max())
 
 
 ### Task2: Data Visualization
@@ -63,35 +67,33 @@ mapper = linear_cmap("P-Rate", bp.magma(len(pos_rate.unique())), pos_rate.min(),
 # Set the initial x_range to be the first 30 days.
 
 TOOLS = "box_select,lasso_select,wheel_zoom,pan,reset,help"
-p = figure(plot_height=300, plot_width=800, 
-            tools=TOOLS, toolbar_location=None, 
-            x_axis_location="above",
-            background_fill_color="#efefef", x_range=(source.data['x'][0], source.data['x'][30]))
-            
-p.scatter(x="x",y="test_num",
+p = figure(plot_height=500, plot_width=1200,
+           tools=TOOLS, x_axis_type="datetime",
+           x_range=(source.data['date'][0], source.data['date'][30]))
+
+p.scatter(x="date",y="test_num",
           source=source,
-          color=linear_cmap('pos_rate', bp.Magma256, pos_rate.min(), pos_rate.max()),
-          fill_alpha=0.5, size=10)
+          color=linear_cmap('pos_rate', bp.Inferno256, pos_rate.min(), pos_rate.max()),
+          fill_alpha=0.5, size=10
+          )
+
 
 p.title.text = 'Covid-19 Tests in Switzerland'
 p.yaxis.axis_label = "Total Tests"
 p.xaxis.axis_label = "Date"
 p.sizing_mode = "stretch_both"
 
-
 # Add a hovertool to display date, total test number
-hover = HoverTool(tooltips=[
-            ('date', '@x{%F}'),
-            ("test", "@test_num")
-        ],
-        formatters={'@x': 'datetime'}
-)
+hover = HoverTool(tooltips=[("date", "@date{%F}"), ("test", "@test_num"), ],  formatters={'@date': 'datetime'})
 p.add_tools(hover)
 
-## T2.2 Add a colorbar to the above scatter plot, and encode positve rate values with colors; please use the color mapper defined in T1.3
+
+
+## T2.2 Add a colorbar to the above scatter plot, and encode positve rate values with colors; please use the color mapper defined in T1.3 
 
 color_bar = ColorBar(color_mapper=mapper['transform'], width=16, location=(0,0), title="P_rate")
 p.add_layout(color_bar, 'right')
+
 
 ## T2.3 Covid-19 Positive Number Plot using RangeTool
 # In this range plot, x axis is the time, and y axis is the positive test number.
@@ -101,14 +103,14 @@ select = figure(title="Drag the middle and edges of the selection box to change 
                 plot_width=1200,
                 x_axis_type="datetime")
 
+
 # Define a RangeTool to link with x_range in the scatter plot
 range_tool = RangeTool(x_range=p.x_range)
 range_tool.overlay.fill_color = "green"
 range_tool.overlay.fill_alpha = 0.2
 
-
 # Draw a line plot and add the RangeTool to the plot
-select.line(x="x",y="pos_num",source=source)
+select.line(x="date",y="pos_num",source=source)
 select.yaxis.axis_label = "Positive Cases"
 select.xaxis.axis_label = "Date"
 select.add_tools(range_tool)
@@ -116,12 +118,14 @@ select.toolbar.active_multi = range_tool
 
 
 # Add a hovertool to the range plot and display date, positive test number
-hover2 = HoverTool(tooltips=[("date", "@x{%F}"), ("pos_num", "@pos_num"), ],  formatters={'@date': 'datetime'})
+hover2 = HoverTool(tooltips=[("date", "@date{%F}"), ("pos_num", "@pos_num"), ],  formatters={'@date': 'datetime'})
 select.add_tools(hover2)
 
 
 ## T2.4 Layout arrangement and display
+
 linked_p = gridplot([[p],[select]])
 show(linked_p)
-output_file("dvc_ex3.html")
-save(linked_p)
+#output_file("dvc_ex3.html")
+#save(linked_p)
+
---
+++ DVC_2020_Exercise4/ex4_play.py
@@ -0,0 +1,233 @@
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+from datetime import datetime,timedelta
+
+import bokeh.palettes as bp
+from bokeh.plotting import figure,curdoc
+from bokeh.transform import linear_cmap
+from bokeh.layouts import column, row
+from bokeh.models import (CDSView, 
+						HoverTool,ColorBar,
+						GeoJSONDataSource, 
+						Patches,
+						RadioButtonGroup,
+						DateSlider,
+						Button)
+
+
+# ====================================================================
+# Goal: Visualize demographics and daily new cases statistics in Switzerland
+
+# The visualization output contains 2 parts: 
+
+# Part 1: add a map view and the color encodes the Density and BedsPerCapita, 
+# with a RadioButtonGroup which controls which aspect to show. 
+
+
+# Part 2: add a circle in each canton on the map, size of circle represents the current daily new cases per capita,
+# add a timeline slider, add callback function controlling the slider and changing the size of the circle on the map when dragging the time slider.
+# Additionally, a "Play" button can animate the slider as well as circles on the map.  
+
+# Reference link
+# https://towardsdatascience.com/walkthrough-mapping-basics-with-bokeh-and-geopandas-in-python-43f40aa5b7e9
+
+# ====================================================================
+
+
+
+### Task 1: Data Preprocessing
+
+## T1.1 Read and filter data 
+# Four data sources:
+# Demographics.csv: the statistics data about population density and hospital beds per capita in each canton
+# covid_19_cases_switzerland_standard_format.csv: the location(longitude, latitude) of the capital city in each canton
+# covid19_cases_switzerland_openzh-phase2.csv: same as in ex2, daily new cases in each canton
+# gadm36_CHE_1.shp: the shape file contains geometry data of swiss cantons, and is provided in the "data" folder. 
+# Please do not submit any data file with your solution, and you can asssume your solution is at the same directory as data 
+
+
+#demo_url = 'https://github.com/daenuprobst/covid19-cases-switzerland/blob/master/demographics.csv'
+#local_url = 'https://github.com/daenuprobst/covid19-cases-switzerland/blob/master/covid_19_cases_switzerland_standard_format.csv'
+#case_url = 'https://github.com/daenuprobst/covid19-cases-switzerland/blob/master/covid19_cases_switzerland_openzh-phase2.csv'
+demo_url = 'https://raw.githubusercontent.com/daenuprobst/covid19-cases-switzerland/master/demographics.csv'
+local_url = 'https://raw.githubusercontent.com/daenuprobst/covid19-cases-switzerland/master/covid_19_cases_switzerland_standard_format.csv'
+case_url = 'https://raw.githubusercontent.com/daenuprobst/covid19-cases-switzerland/master/covid19_cases_switzerland_openzh-phase2.csv'
+shape_dir = 'data/gadm36_CHE_1.shp'
+
+# Read from demo_url into a dataframe using pandas
+demo_raw = pd.read_csv(demo_url)
+#print(demo_raw.head(5))
+
+# Read from local_url into a dataframe using pandas
+local_raw = pd.read_csv(local_url)
+#print(local_raw.head(5))
+
+# Extract unique 'abbreviation_canton','lat','long' combinations from local_raw
+canton_point = local_raw.groupby(['abbreviation_canton','lat','long']).size().reset_index()
+#print(canton_point)
+
+
+# Read from case_url into a dataframe using pandas
+case_raw = pd.read_csv(case_url)
+#print(case_raw.head(5))
+# Create a date list from case_raw and convert to datatime form
+dates = pd.to_datetime(case_raw['Date']).tolist()
+#print(dates)
+
+
+# Read shape file from shape_dir using geopandas
+shape_raw = gpd.read_file(shape_dir)
+#print(shape_raw)
+
+# Extract canton name abbreviations from the attribute 'HASC_1', e.g. CH.AG --> AG, CH.ZH --> ZH
+# And save into a new column named 'Canton' 
+shape_raw['Canton'] = shape_raw['HASC_1'].str[-2:]
+#print(shape_raw.Canton)
+canton_poly = shape_raw[['geometry','Canton']]
+
+## T1.2 Merge data and build a GeoJSONDataSource
+
+# Merge canton_poly with demo_raw on attribute name 'Canton' into dataframe merged,
+# then merge the result with canton_point on 'Canton' and 'abbreviation_canton' respectively
+
+
+# Potential issue: 
+# https://stackoverflow.com/questions/57045479/is-there-a-way-to-fix-maximum-recursion-level-in-python-3
+merged = demo_raw.merge(canton_poly, on='Canton')
+merged = merged.merge(canton_point, left_on='Canton', right_on='abbreviation_canton')
+#print(merged)
+
+# For each date, extract a list of daily new cases per capita from all cantons(e.g. 'AG_diff_pc', 'AI_diff_pc', etc.), and add as a new column in merged
+# For instance, in the column['2020-10-31'], there are: [0.0005411327220155498, nan, nan, 0.000496300306803826, ...]
+dpc = case_raw.loc[:, case_raw.columns.str.contains('diff_pc')].drop(columns='CH_diff_pc')
+for i,d in enumerate(dates):
+	dnc = dpc.iloc[i].tolist()
+	merged[d] = dnc
+
+# Calculate circle sizes that are proportional to dnc per capita
+# Set the latest dnc as default 
+merged['size'] = merged.iloc[:,-1]*1e5/5+10
+merged['dnc'] = merged.iloc[:,-1]
+
+# Build a GeoJSONDataSource from merged
+geosource = GeoJSONDataSource(geojson=merged.to_json())
+
+print(merged)
+
+'''
+# Task 2: Data Visualization
+
+# T2.1 Create linear color mappers for 2 attributes in demo_raw: population density, hospital beds per capita 
+# Map their maximum values to the high, and mimimum to the low
+labels = ['Density','BedsPerCapita']
+
+mappers = {} 
+mappers['Density'] = ...
+mappers['BedsPerCapita'] = ...
+
+
+# T2.2 Draw a Switzerland Map on canton level
+
+# Define a figure 
+p1 = figure(title = 'Demographics in Switzerland', 
+					 plot_height = 600 ,
+					 plot_width = 950, 
+					 toolbar_location = 'above',
+					 tools = "pan, wheel_zoom, box_zoom, reset")
+
+p1.xgrid.grid_line_color = None
+p1.ygrid.grid_line_color = None
+
+# Plot the map using patches, set the fill_color as mappers['Density']
+cantons = p1.patches(...)
+
+
+# Create a colorbar with mapper['Density'] and add it to above figure
+color_bar = ColorBar(...)
+p1.add_layout(color_bar, 'right')
+
+
+# Add a hovertool to display canton, density, bedspercapita and dnc 
+hover = HoverTool(tooltips=[...] ,renderers=[cantons])
+
+p1.add_tools(hover)
+
+
+
+# T2.3 Add circles at the locations of capital cities for each canton, and the sizes are proportional to daily new cases per capita
+sites = p1.circle(...)
+
+
+# T2.4 Create a radio button group with labels 'Density', and 'BedsPerCapita'
+buttons = RadioButtonGroup(...)
+
+# Define a function to update color mapper used in both patches and colorbar 
+def update_bar(new):
+	for i,d in enumerate(labels):
+		if i == new:
+			...
+
+
+buttons.on_click(...)
+
+
+# T2.5 Add a dateslider to control which per capita daily new cases information to display
+
+# Define a dateslider using maximum and mimimum dates, set value to be the latest date
+timeslider = DateSlider(...)
+
+# Complete the callback function 
+# Hints: 
+# 	convert the timestamp value from the slider to datetime and format it in the form of '%Y-%m-%d'
+#	update columns 'size', 'dnc' with the column named '%Y-%m-%d' in merged
+#	update geosource with new merged
+
+def callback(attr,old,new):
+	# Convert timestamp to datetime
+	# https://stackoverflow.com/questions/9744775/how-to-convert-integer-timestamp-to-python-datetime
+	date = ...
+	i = date.strftime('%Y-%m-%d')
+
+	merged.size = ...
+	merged.dnc = ...
+	geosource.geojson = ...
+
+
+# Circles change on mouse move
+timeslider.on_change(...)
+
+
+# T2.6 Add a play button to change slider value and update the map plot dynamically
+# https://stackoverflow.com/questions/46420606/python-bokeh-add-a-play-button-to-a-slider
+# https://stackoverflow.com/questions/441147/how-to-subtract-a-day-from-a-date
+
+# Update the slider value with one day before current date
+def animate_update_slider():
+	# Extract date from slider's current value 
+	date = ...
+	# Subtract one day from date and do not exceed the allowed date range
+	day = ...
+
+	...
+
+	timeslider.value = day
+
+# Define the callback function of button
+def animate():
+	global callback_id
+	if button.label == '► Play':
+		button.label = '❚❚ Pause'
+		callback_id = curdoc().add_periodic_callback(animate_update_slider, 500)
+	else:
+		button.label = '► Play'
+		curdoc().remove_periodic_callback(callback_id)
+
+button = Button(label='► Play', width=80, height=40)
+button.on_click(animate)
+
+
+
+
+curdoc().add_root(column(p1,buttons, row(timeslider,button)))
+'''
